ce2586e26532304c866860b0bf29c6a89402554c
devJerb <gutierrez.jeromebryant@gmail.com>
2025-01-20 15:58:00 +0800

    fix: hide base url on axios

--- src/components/api/axios-instance.ts
+++ src/components/api/axios-instance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "https://stripe-billing-service-production.up.railway.app",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
